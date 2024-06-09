b19fc7b34c9c878da0b2b8060d83530066292957
Serhii Borysenko <borisenko37@gmail.com>
2024-06-10 00:11:45 +0200

    add html

---
+++ src/js/1-timer.js
@@ -0,0 +1,11 @@
+const startTime = Date.now();
+
+// Твій код, виконуваний упродовж деякого часу
+for (let i = 0; i <= 100; i += 1) {
+  console.log(i);
+}
+
+const endTime = Date.now();
+const elapsedTime = endTime - startTime;
+
+console.log(`Elapsed time: ${elapsedTime} ms`);
