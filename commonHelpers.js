1fc79e2cd426d9e980e1643148cbc504b5c54112
BorysenkoS <110692205+BorysenkoS@users.noreply.github.com>
2024-06-09 22:12:20 +0000

    Deploying to gh-pages from @ BorysenkoS/goit-js-hw-10@b19fc7b34c9c878da0b2b8060d83530066292957 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const e=Date.now();for(let o=0;o<=100;o+=1)console.log(o);const t=Date.now(),s=t-e;console.log(`Elapsed time: ${s} ms`);
+//# sourceMappingURL=commonHelpers.js.map
