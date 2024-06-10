8ae8b7cd35dbbe1e76176e1d8219ffcbbe19bcdf
Serhii Borysenko <borisenko37@gmail.com>
2024-06-10 22:58:32 +0200

    add styles for timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,11 +1,59 @@
-const startTime = Date.now();
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-// Твій код, виконуваний упродовж деякого часу
-for (let i = 0; i <= 100; i += 1) {
-  console.log(i);
+let userSelectedDate;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr('#datetime-picker', options, {});
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  timer: document.querySelector('.timer'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.startBtn.addEventListener('click', () => {
+  iziToast.show({
+    iconUrl: '../img/javascript.svg',
+    title: 'Error',
+    titleColor: 'white',
+    message: 'Illegal operation',
+    messageColor: 'white',
+    messageSize: '16px',
+    backgroundColor: '#ef4040',
+    position: 'topRight',
+    theme: 'dark',
+  });
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
 }
-
-const endTime = Date.now();
-const elapsedTime = endTime - startTime;
-
-console.log(`Elapsed time: ${elapsedTime} ms`);
