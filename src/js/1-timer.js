a31fd8927bff82deee228458da6ef8bdd907df01
Serhii Borysenko <borisenko37@gmail.com>
2024-06-11 00:19:58 +0200

    add some styles for snackbar

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,59 +1,68 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
+// import flatpickr from 'flatpickr';
+// import 'flatpickr/dist/flatpickr.min.css';
+// import iziToast from 'izitoast';
+// import 'izitoast/dist/css/iziToast.min.css';
 
-let userSelectedDate;
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+// const refs = {
+//   startBtn: document.querySelector('[data-start]'),
+//   timer: document.querySelector('.timer'),
+//   days: document.querySelector('[data-days]'),
+//   hours: document.querySelector('[data-hours]'),
+//   minutes: document.querySelector('[data-minutes]'),
+//   seconds: document.querySelector('[data-seconds]'),
+// };
 
-flatpickr('#datetime-picker', options, {});
+// let userSelectedDate;
 
-const refs = {
-  startBtn: document.querySelector('[data-start]'),
-  timer: document.querySelector('.timer'),
-  days: document.querySelector('[data-days]'),
-  hours: document.querySelector('[data-hours]'),
-  minutes: document.querySelector('[data-minutes]'),
-  seconds: document.querySelector('[data-seconds]'),
-};
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//   },
+// };
 
-refs.startBtn.addEventListener('click', () => {
-  iziToast.show({
-    iconUrl: '../img/javascript.svg',
-    title: 'Error',
-    titleColor: 'white',
-    message: 'Illegal operation',
-    messageColor: 'white',
-    messageSize: '16px',
-    backgroundColor: '#ef4040',
-    position: 'topRight',
-    theme: 'dark',
-  });
-});
+// flatpickr('#datetime-picker', options, {});
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+// refs.startBtn.addEventListener('click', () => {
+//   iziToast.show({
+//     iconUrl: '../img/javascript.svg',
+//     title: 'Error',
+//     titleColor: 'white',
+//     message: 'Illegal operation',
+//     messageColor: 'white',
+//     messageSize: '16px',
+//     backgroundColor: '#ef4040',
+//     position: 'topRight',
+//     theme: 'dark',
+//   });
+// });
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
 
-  return { days, hours, minutes, seconds };
-}
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+const timerId = setTimeout(() => {
+  console.log(timerId);
+}, 4000);
+
+const intId = setTimeout(() => {
+  clearTimeout(timerId);
+}, 3000);
